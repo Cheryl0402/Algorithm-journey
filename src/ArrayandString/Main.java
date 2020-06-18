fe48a2a6da5e4620d694cd05a53da6dbf39fe50a
cheryl0402 <yi.cheryl.chen@gmail.com>
2020-06-18 15:22:32 -0700

    delete unnecssary files Please enter the commit message for your changes. Lines starting

--- src/ArrayandString/Main.java
+++ src/ArrayandString/Main.java
@@ -9,21 +9,40 @@ public class Main {
         int[] testArray2 = {1, 2, 9};
         int[] testArray3 = {9,9,9};
 
-        System.out.println("The test array 0 plus one is: ");
-        for (int item : plusOne(testArray0))
-            System.out.print(item + " ");
+//        System.out.println("The test array 0 plus one is: ");
+//        for (int item : plusOne(testArray0))
+//            System.out.print(item + " ");
+//
+//        System.out.println("\n" + "The test array 1 plus one is: ");
+//        for (int item : plusOne(testArray1))
+//            System.out.print(item + " ");
+//
+//        System.out.println("\n" + "The test array 2 plus one is: ");
+//        for (int item : plusOne(testArray2))
+//            System.out.print(item + " ");
+//
+//        System.out.println("\n" + "The test array 3 plus one is: ");
+//        for (int item : plusOne(testArray3))
+//            System.out.print(item + " ");
 
-        System.out.println("\n" + "The test array 1 plus one is: ");
-        for (int item : plusOne(testArray1))
-            System.out.print(item + " ");
+        System.out.println("");
+        int[] myArr = {1,2,3,4,5,6,7};
+        rotate(myArr,8);
+        for(int i : myArr){
+            System.out.println(i + " ");
+        }
+    }
 
-        System.out.println("\n" + "The test array 2 plus one is: ");
-        for (int item : plusOne(testArray2))
-            System.out.print(item + " ");
+    public static void rotate(int[] nums, int k) {
+      int len = nums.length;
+      k = k % len;
+      if(k == 0) return;
 
-        System.out.println("\n" + "The test array 3 plus one is: ");
-        for (int item : plusOne(testArray3))
-            System.out.print(item + " ");
+      int[] resultArr = new int[nums.length];
+      int m = 0;
+      for(int i = 0; i > 0; i--){
+
+      }
     }
 
     public static int[] plusOne(int[] digits) {
